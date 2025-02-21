e7d2ec2adf3cd02b898d257e86619b79812f2626
sherif003 <sherif.karam2003@gmail.com>
2025-02-21 16:51:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from products import products_page
 from orders import orders_page
 from reports import reports_page
+from Refund Page import refund_page
 
 st.sidebar.title("Stota Store POS System")
 page = st.sidebar.radio("Select Page", [ "Orders","Products", "Reports & Cash Breakdown","Refund Page"])
