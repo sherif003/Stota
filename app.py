a1c0f52d95410959f37623c3125d604ae92ef71a
sherif003 <sherif.karam2003@gmail.com>
2025-02-21 16:55:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import streamlit as st
 from products import products_page
 from orders import orders_page
 from reports import reports_page
-from refund_page import refund_page
+from refund import refund_page
 
 st.sidebar.title("Stota Store POS System")
-page = st.sidebar.radio("Select Page", [ "Orders","Products", "Reports & Cash Breakdown","Refund Page"])
+page = st.sidebar.radio("Select Page", [ "Orders","Products", "Reports & Cash Breakdown","Refund"])
 
 if page == "Products":
     products_page()
@@ -13,5 +13,5 @@ elif page == "Orders":
     orders_page()
 elif page == "Reports & Cash Breakdown":
     reports_page()
-elif page == "Refund Page":
+elif page == "Refund":
     refund_page()
